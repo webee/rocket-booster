1fcaa72cba3a25a55319c02e0e55994b2eb518a6
Mark Li <limarksmail@gmail.com>
2021-08-26 16:54:58 +0800

    refactor

--- src/storage.ts
+++ src/storage.ts
@@ -1,52 +1,38 @@
-import {PersistOptions, AsyncStorage } from '../types/storage'
+import { AsyncStorage, CloudflareKVConfig } from '../types/storage'
 
-export class KVStorage<S> implements PersistOptions<S> {
-    public storage: AsyncStorage | undefined
-    public restoreState: (key: string, storage?: AsyncStorage | Storage) => Promise<S> | S
-    public saveState: (key: string, state: {}, storage?: AsyncStorage | Storage) => Promise<void> | void
+export class CloudflareKVStorage implements AsyncStorage {
+  private defaultData = {}
 
-    public constructor(options?: PersistOptions<S>) {
-        if (typeof options === 'undefined') options = {} as PersistOptions<S>
-        /**
-         * 1. First, prefer storage sent in optinos
-         * 2. Otherwise, use window.localStorage if available
-         * 3. Finally, try to use MockStorage
-         * 4. None of above? Well we gotta fail.
-         */
-        if (options.storage) { this.storage = options.storage }
-        else { this.storage =  }
+  private setCache = <T>(key: string, data: T) => DATABASE.put(key, data)
+  private getCache = (key: string) => DATABASE.get(key)
+  private removeCache = (key: string) => DATABASE.delete(key)
 
-        /**
-       * Async {@link #VuexPersistence.restoreState} implementation
-       * @type {((key: string, storage?: Storage) =>
-       *      (Promise<S> | S)) | ((key: string, storage: AsyncStorage) => Promise<any>)}
-         */
-        this.restoreState = (
-          (options.restoreState != null)
-            ? options.restoreState
-            : ((key: string, storage: AsyncStorage) =>
-              (storage).getItem(key)
-                .then((value) => (value || {})
-                )
-            )
-        )
-  
-        /**
-         * Async {@link #VuexPersistence.saveState} implementation
-         * @type {((key: string, state: {}, storage?: Storage) =>
-         *    (Promise<void> | void)) | ((key: string, state: {}, storage?: Storage) => Promise<void>)}
-         */
-        this.saveState = (
-          (options.saveState != null)
-            ? options.saveState
-            : ((key: string, state: {}, storage: AsyncStorage) =>
-              (storage).setItem(
-                key, // Second argument is state _object_ if asyc storage, stringified otherwise
-                // do not stringify the state if the storage type is async
-                (merge({}, state || {}, this.mergeOption))
-              )
-            )
-        )
-  
+  async getItem(key: string) {
+    const cache = await this.getCache(key)
+    if (!cache) {
+      await this.setCache(key, JSON.stringify(this.defaultData))
+      return this.defaultData
+    } else {
+      return JSON.parse(cache)
     }
+  }
+
+  async removeItem(key: string) {
+    const cache = await this.getCache(key)
+    if (!cache) {
+      return false
+    } else {
+      this.removeCache(key)
+      return true
+    }
+  }
+
+  async setItem<T>(key: string, value: T) {
+    try {
+      await this.setCache(key, value)
+      return true
+    } catch (err) {
+      return false
+    }
+  }
 }
