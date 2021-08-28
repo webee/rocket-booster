29fb9401bd97f71d7c34ce12584581f6ddec3a2c
Mark Li <limarksmail@gmail.com>
2021-08-28 20:58:58 +0800

    updates

--- src/storage.ts
+++ src/storage.ts
@@ -1,38 +1,38 @@
-import { AsyncStorage, CloudflareKVConfig } from '../types/storage'
+import { AsyncStorage } from '../types/storage';
 
 export class CloudflareKVStorage implements AsyncStorage {
   private defaultData = {}
 
   private setCache = <T>(key: string, data: T) => DATABASE.put(key, data)
+
   private getCache = (key: string) => DATABASE.get(key)
+
   private removeCache = (key: string) => DATABASE.delete(key)
 
-  async getItem(key: string) {
-    const cache = await this.getCache(key)
+  async getItem<T>(key: string): Promise<T | undefined> {
+    const cache = await this.getCache(key);
     if (!cache) {
-      await this.setCache(key, JSON.stringify(this.defaultData))
-      return this.defaultData
-    } else {
-      return JSON.parse(cache)
+      await this.setCache(key, JSON.stringify(this.defaultData));
+      return undefined;
     }
+    return JSON.parse(cache);
   }
 
-  async removeItem(key: string) {
-    const cache = await this.getCache(key)
+  async removeItem(key: string): Promise<boolean> {
+    const cache = await this.getCache(key);
     if (!cache) {
-      return false
-    } else {
-      this.removeCache(key)
-      return true
+      return false;
     }
+    await this.removeCache(key);
+    return true;
   }
 
-  async setItem<T>(key: string, value: T) {
+  async setItem<T>(key: string, value: T): Promise<boolean> {
     try {
-      await this.setCache(key, value)
-      return true
+      await this.setCache(key, JSON.stringify(value));
+      return true;
     } catch (err) {
-      return false
+      return false;
     }
   }
 }
--- types/storage.ts
+++ types/storage.ts
@@ -2,46 +2,7 @@ export interface AsyncStorage {
   _config?: {
       name: string
   }
-  getItem<T>(key: string): Promise<T>
-  setItem<T>(key: string, data: T): Promise<T>
-  removeItem<T>(key: string): Promise<T>
-}
-
-/**
- * Options to be used to construct a {@link VuexPersistence} object
- */
- 
-export interface PersistOptions<S> {
-  /**
-  * Window.Storage type object. Default is localStorage
-  */
-  storage?: AsyncStorage
-
-  /**
-  * Method to retrieve state from persistence
-  * @param key
-  * @param [storage]
-  */
-  restoreState?: (key: string, storage?: AsyncStorage) => Promise<S> | S
-
-  /**
-  * Method to save state into persistence
-  * @param key
-  * @param state
-  * @param [storage]
-  */
-  saveState?: (key: string, state: {}, storage?: AsyncStorage) => Promise<void> | void
-
-  /**
-  * Key to use to save the state into the storage
-  */
-  key?: string
-
-  /**
-  * If your storage is async
-  * i.e., if setItem(), getItem() etc return Promises
-  * (Must be used for asynchronous storages like LocalForage)
-  * @default false
-  */
-  asyncStorage?: boolean
+  getItem<T>(key: string): Promise<T | undefined>
+  setItem<T>(key: string, data: T): Promise<boolean>
+  removeItem(key: string): Promise<boolean>
 }
